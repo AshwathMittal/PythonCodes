b0cc6ece8a15979c915081e67dbc53bc8b7378e3
AshwathMittal <ashwathmittal@gmail.com>
2021-10-31 14:44:34 +0530

    Not In a mood to write commit message

---
+++ PythonProjects/GuessTheNumber.py
@@ -0,0 +1,22 @@
+import random
+import sys
+a = random.randint(1, 10)
+
+score = 0
+
+while True:
+    try:
+        guess = int(input('Guess a number between 1 and 10: '))
+    except ValueError:
+        print('Please enter a number')
+    if guess == a:
+        print("You guessed it!")
+        score += 1
+        print("Your score is:", score)
+    elif guess == 0:
+        print("Your score is:", score)
+        sys.exit()
+    elif guess < a:
+        print("Too low!")
+    elif guess > a:
+        print("Too high!")
--- enumerate.py
+++ enumerate.py
@@ -1,17 +1,8 @@
-# z = [2,6,2,' ',8,]
-# for i,v in enumerate(z):
-#     print(v, ":",i)
-#
-
-
-def count(let):
-    l = 0
-    u = 0
-    for i in let:
-        if i.islower():
-            l = l +1
-
-        elif i.isupper():
-            u = u +1
-    return f"UpperCase Letters: {u}, LowerCase Letters: {l}"
-print(count("PyThON"))
+z = [2,6,2,'3',8,]
+for v,i in enumerate(z):
+    print(v,":",i)
+# enumerate() is a function that returns an enumerate object.
+# It takes a sequence as its argument and returns an enumerate object.
+# The enumerate object contains a pair of values for each index position in the sequence.
+# The first value is the index position and the second value is the value at that index position.
+# The enumerate object can be used to loop through the sequence.
