a1e882e0f19fe9728768c5e331d784a9d0afe7b4
Ashwath Mittal <68093378+AshwathMittal@users.noreply.github.com>
2023-02-21 09:45:57 +0530

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -1,6 +1,8 @@
 z = [2,6,2,'3',8,]
 for v,i in enumerate(z):
     print(v,":",i)
+    
+    
 # enumerate() is a function that returns an enumerate object.
 # It takes a sequence as its argument and returns an enumerate object.
 # The enumerate object contains a pair of values for each index position in the sequence.
